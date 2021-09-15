accdc4af48865e4a8e2f20b556d3ab75d6d0d37e
kabatech255 <risk.examination@gmail.com>
2021-09-16 02:52:05 +0900

    feat: ラベル変更

--- interfaces/common/definitionList/MeetingRecordTermKey.ts
+++ interfaces/common/definitionList/MeetingRecordTermKey.ts
@@ -5,7 +5,7 @@ export interface MeetingRecordTermKey extends DefinitionTermKey {
   title: '会議名'
   meeting_date: '開催日時'
   place: '開催場所'
-  summary: '概要'
+  summary: '議題'
   members: '参加者'
   decisions: '決定事項'
 }
