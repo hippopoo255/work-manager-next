27ff8d413faaded1da31c8b63cecd3e9004d03f8
kabatech255 <risk.examination@gmail.com>
2021-11-22 15:33:54 +0900

    feat: AppSyncからのUnauthorizationデータ変更

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from 'next/app'
 import theme from '@/theme'
 import React from 'react'
 import { AuthProvider } from '@/provider'
+import { Auth0Provider } from '@auth0/auth0-react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
@@ -14,11 +15,20 @@ function MyApp({ Component, pageProps }: AppProps) {
       jssStyles.parentElement?.removeChild(jssStyles)
     }
   }, [])
+  const redirectUri = process.env.NEXT_PUBLIC_BASE_URL || ''
+  const domainObj = {
+    domain: process.env.NEXT_PUBLIC_AUTH0_DOMAIN || '',
+    clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID || '',
+    // audience: process.env.NEXT_PUBLIC_AUTH0_AUTHORIZER_IDENTIFIER || '',
+    redirectUri,
+  }
 
   return (
     <ThemeProvider theme={theme}>
       <AuthProvider>
-        <Component {...pageProps} />
+        <Auth0Provider {...domainObj}>
+          <Component {...pageProps} />
+        </Auth0Provider>
       </AuthProvider>
     </ThemeProvider>
   )
