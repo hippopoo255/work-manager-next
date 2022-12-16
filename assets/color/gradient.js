2444a4980420579a34de2c08988caf8e4bfdbf5e
hippopoo255 <rcr126126@gmail.com>
2022-12-16 17:26:43 +0900

    feat: backup  as v1

--- assets/color/gradient.js
+++ assets/color/gradient.js
@@ -5,7 +5,7 @@ export const linerGradient = {
   blue: 'linear-gradient(315deg,#25AAE1,#04BEFE,#3f86ED)',
   primary: 'linear-gradient(135deg,#459522,#B1EA4D)',
   colder: 'linear-gradient(135deg,#00a8c5,#97e021)',
-  secondary: 'linear-gradient(135deg,#1583dd,#00c990)',
+  secondary: 'linear-gradient(to top left,#6684f7,#04b0fb)',
   error: 'linear-gradient(135deg, #EB3941, #F15E64, #E2373f)',
   success: 'linear-gradient(315deg,#459522,#53d01f,#459522)',
 }
